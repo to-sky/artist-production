442df69258bea767debd8424610357a333e051d5
Mike <sirnec@gmail.com>
2019-05-08 13:43:15 +0600

    i18n: fill missing lines

--- app/Modules/Admin/resources/lang/en/models.php
+++ app/Modules/Admin/resources/lang/en/models.php
@@ -1,4 +1,18 @@
 <?php
 return [
-    'Client' => 'Client'
+    'Client' => 'Client',
+    'client' => 'Client',
+    'City' => 'City',
+    'Event' => 'Event',
+    'Building' => 'Building',
+    'Hall' => 'Hall',
+    'Shipping' => 'Shipping',
+    'shipping' => 'shipping',
+    'Shipping zone' => 'Shipping zone',
+    'shipping zones' => 'shipping zones',
+    'Country' => 'Country',
+    'Countries' => 'Countries',
+    'role' => 'role',
+    'user' => 'user',
+    'menu' => 'menu'
 ];
