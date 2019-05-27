78d93cfb212e62e18c779896d5cc889963e36e4f
Dmitry Tolsky <dmitry.tolsky@rhinoda.com>
2019-05-27 15:01:17 +0600

    fixed ticket barcode

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Keygen\Keygen;
 
 class Ticket extends Model
 {
@@ -13,8 +14,18 @@ class Ticket extends Model
     const RESERVED = 1;
     const SOLD = 2;
 
+    public static $barcodes;
+
     protected $fillable = ['barcode', 'amount_printed', 'price', 'status', 'user_id', 'event_id', 'place_id', 'price_id'];
 
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (! $this->barcodes){
+            $this->generateUniqueBarcode();
+        }
+    }
 
     public function user()
     {
@@ -39,5 +50,18 @@ class Ticket extends Model
     public function scopeAvailable($query)
     {
         return $query->where('status', self::AVAILABLE);
+    }
+
+    public function generateUniqueBarcode()
+    {
+        if (isset(self::$barcodes)) {
+            self::$barcodes = Ticket::withTrashed()->pluck('id', 'barcode');
+        }
+
+        do {
+            $barcode = Keygen::numeric(12)->generate();
+        } while (isset(self::$barcodes[$barcode]));
+
+        $this->barcode = $barcode;
     }
 }
