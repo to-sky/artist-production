a74ef2efa2a4d2cd693d55acac638d674d139d83
Mike <sirnec@gmail.com>
2019-07-10 12:11:40 +0600

    PaymentMethodsSeeder fix

--- database/data/payment_methods.php
+++ database/data/payment_methods.php
@@ -6,11 +6,13 @@ return [
         'active' => 1,
         'price_type' => \App\Models\PaymentMethod::PRICE_TYPE_PERCENTAGE,
         'price_amount' => 0,
+        'payment_type' => \App\Models\PaymentMethod::TYPE_DELAY,
     ],
     [
         'name' => 'Paypal',
         'active' => 1,
         'price_type' => \App\Models\PaymentMethod::PRICE_TYPE_PERCENTAGE,
         'price_amount' => 3,
+        'payment_type' => \App\Models\PaymentMethod::TYPE_INSTANT,
     ]
 ];
