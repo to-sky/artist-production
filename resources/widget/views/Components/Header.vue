a9ed924418be76316c1a5269461c01e6468b9f21
Mike <sirnec@gmail.com>
2019-10-27 06:34:57 +0600

    fixes: widget header

--- resources/widget/views/Components/Header.vue
+++ resources/widget/views/Components/Header.vue
@@ -1,90 +1,95 @@
 <template>
   <div class="header-event">
     <img class="header-event__image" :src="event.raw('event.event_image.file_url')" :alt="event.raw('event.name')">
-    <div class="header-event__description">
-      <h2 class="header-event__title">{{ event.raw('event.name') }}</h2>
-      <a
-        :href="'https://maps.google.com/?q=' + event.raw('hall.building.address')"
-        class="header-event__link-google"
-        target="_blank"
-      >
-        <div class="header-event__marker-icon"></div>
-        <p class="header-event__text">{{ event.raw('hall.name') }}, {{ event.raw('hall.building.city.name') }}</p>
-      </a>
-    </div>
-    <div class="header-event__date" v-if="!isMobile">
-      <div class="calendar-container">
-        <div class="date-container">
-          <div class="calendar__icon"></div>
-          <span class="calendar__end-day">{{ endDay }}</span>
-          <div class="calendar__end-month">{{ endMonth }}</div>
-          <div class="calendar-year-container">
-            <span class="calendar__end-day-name">{{ endDayName }}</span>
-            <div class="calendar__end-year">{{ endYear }}</div>
+
+    <div class="header-event__description-container">
+      <div class="header-event__title-container">
+        <h2 class="header-event__title">{{ event.raw('event.name') }}</h2>
+      </div>
+      <div class="header-event__description">
+        <a
+            :href="'https://maps.google.com/?q=' + event.raw('hall.building_address')"
+            class="header-event__link-google"
+            target="_blank"
+        >
+          <div class="header-event__marker-icon"></div>
+          <p class="header-event__text">{{ event.raw('hall.name') }}, {{ event.raw('hall.building_name') }}</p>
+        </a>
+      </div>
+      <div class="header-event__date" v-if="!isMobile">
+        <div class="calendar-container">
+          <div class="date-container">
+            <div class="calendar__icon"></div>
+            <span class="calendar__end-day">{{ endDay }}</span>
+            <div class="calendar__end-month">{{ endMonth }}</div>
+            <div class="calendar-year-container">
+              <span class="calendar__end-day-name">{{ endDayName }}</span>
+              <div class="calendar__end-year">{{ endYear }}</div>
+            </div>
+          </div>
+          <div class="time-container">
+            <div class="calendar__icon-time"></div>
+            <span class="calendar__end-hour">{{ endHour }}</span>
           </div>
         </div>
-        <div class="time-container">
-          <div class="calendar__icon-time"></div>
-          <span class="calendar__end-hour">{{ endHour }}</span>
+      </div>
+
+      <div class="select-language-container" v-if="!isMobile">
+        <div
+            :class="'select-language' + ' select-language-change__' + getLangName"
+            @click="showLanguages = !showLanguages"
+        ></div>
+        <div class="select-language-change" v-show="showLanguages">
+          <div
+              class="select-language__language select-language-change__en"
+              @click="changeLanguage('en')"
+          ></div>
+          <div
+              class="select-language__language select-language-change__de"
+              @click="changeLanguage('de')"
+          ></div>
+          <div
+              class="select-language__language select-language-change__ru"
+              @click="changeLanguage('ru')"
+          ></div>
         </div>
       </div>
-    </div>
 
-    <div class="select-language-container" v-if="!isMobile">
-      <div
-        :class="'select-language' + ' select-language-change__' + getLangName"
-        @click="showLanguages = !showLanguages"
-      ></div>
-      <div class="select-language-change" v-show="showLanguages">
-        <div
-          class="select-language__language select-language-change__en"
-          @click="changeLanguage('en')"
-        ></div>
-        <div
-          class="select-language__language select-language-change__de"
-          @click="changeLanguage('de')"
-        ></div>
-        <div
-          class="select-language__language select-language-change__ru"
-          @click="changeLanguage('ru')"
-        ></div>
-      </div>
-    </div>
-
-    <div class="header-event__date" v-if="isMobile">
-      <div class="calendar-container">
-        <div class="date-container">
-          <div class="calendar__icon"></div>
-          <span class="calendar__end-day">{{ endDay }}</span>
-          <div class="calendar__end-month">{{ endMonthShort }}</div>
-          <div class="calendar-year-container">
-            <span class="calendar__end-day-name">{{ endDayName }}</span>
-            <div class="calendar__end-year">{{ endYear }}</div>
+      <div class="header-event__date" v-if="isMobile">
+        <div class="calendar-container">
+          <div class="date-container">
+            <div class="calendar__icon"></div>
+            <span class="calendar__end-day">{{ endDay }}</span>
+            <div class="calendar__end-month">{{ endMonthShort }}</div>
+            <div class="calendar-year-container">
+              <span class="calendar__end-day-name">{{ endDayName }}</span>
+              <div class="calendar__end-year">{{ endYear }}</div>
+            </div>
           </div>
-        </div>
-        <div class="time-container">
-          <div class="calendar__icon-time"></div>
-          <span class="calendar__end-hour">{{ endHour }}</span>
-          <div class="select-language-container">
-            <div
-              :class="
+          <div class="time-container">
+            <div class="calendar__icon-time"></div>
+            <span class="calendar__end-hour">{{ endHour }}</span>
+            <div class="select-language-container">
+              <div
+                  :class="
                 'select-language' + ' select-language-change__' + getLangName
               "
-              @click="showLanguages = !showLanguages"
-            ></div>
-            <div class="select-language-change" v-show="showLanguages">
-              <div
-                class="select-language__language select-language-change__en"
-                @click="changeLanguage('en')"
+                  @click="showLanguages = !showLanguages"
               ></div>
-              <div
-                class="select-language__language select-language-change__de"
-                @click="changeLanguage('de')"
-              ></div>
-              <div
-                class="select-language__language select-language-change__ru"
-                @click="changeLanguage('ru')"
-              ></div>
+              <div class="select-language-change" v-show="showLanguages">
+                <div
+                    class="select-language__language select-language-change__en"
+                    @click="changeLanguage('en')"
+                ></div>
+                <div
+                    class="select-language__language select-language-change__de"
+                    @click="changeLanguage('de')"
+                ></div>
+                <div
+                    class="select-language__language select-language-change__ru"
+                    @click="changeLanguage('ru')"
+                ></div>
+              </div>
             </div>
           </div>
         </div>
